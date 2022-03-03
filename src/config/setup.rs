6d4a99b0538a91ce47762da7fc3c289af239e5f3
Kazuo Yagi <kazuo.yagi@gmail.com>
2022-03-04 02:07:20 +0900

    Update misc

--- src/config/setup.rs
+++ src/config/setup.rs
@@ -55,24 +55,17 @@ pub fn get<P: AsRef<Path>>(path: P) -> Result<Config, Box<dyn Error>> {
     Ok(c)
 }
 
-pub fn get_endpoint <P: AsRef<Path>>(path: P) -> Result<Config, Box<dyn Error>> {
-    // Open the file in read-only mode with buffer.
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-
-    // Read the JSON contents of the file as an instance of `Config`.
-    let c: Config = serde_json::from_reader(reader)?;
-
+pub fn get_endpoint (c: &Config) -> Result<Vec<Endpoint>, Box<dyn Error>> {
+    let mut endpoints: Vec<Endpoint> = Vec::new();
     for r in &c.github.repositories {
-        let url = format!("https://api.zenhub.com/p2/workspaces/{}/repositories", c.zenhub.workspace_id);
-        let url = format!("{}/{}/board", url, r.repository_id);
-        let url: Result<Url, ParseError> = Url::parse(&url);
-        let ep: Endpoint = Endpoint { github: &c.github, zenhub: &c.zenhub, url };
-        println!("{}", ep.url.unwrap());
+        let url = format!("https://api.zenhub.com/p2/workspaces/{}/repositories", &c.zenhub.workspace_id);
+        let url2 = format!("{}/{}/board", url, &r.repository_id);
+        let url3: Result<Url, ParseError> = Url::parse(&url2);
+        endpoints.push(Endpoint { github: &c.github, zenhub: &c.zenhub, url: url3 });
     }
 
-    // Return the `Config`.
-    Ok(c)
+    // Return the `Endpoint`.
+    Ok(endpoints)
 }
 
 pub fn create_zenhub_endpoint(id: &str, repositories: &Vec<Repository>) -> [Vec<String>; 2] {
